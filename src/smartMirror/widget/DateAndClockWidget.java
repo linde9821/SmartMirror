aad27d1ea57ca587e53378b7286d98e785e6d512
Marvi <Marvi@192.168.0.11>
2019-05-01 00:54:45 +0200

    date changes

--- src/smartMirror/widget/DateAndClockWidget.java
+++ src/smartMirror/widget/DateAndClockWidget.java
@@ -2,30 +2,53 @@ package smartMirror.widget;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.time.LocalTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import smartMirror.DateAndTime.DateHandler;
 import smartMirror.Location.Area;
+import smartMirror.SMPanel.SMPanel;
 
 public class DateAndClockWidget extends Widget {
 
 	DateHandler dh;
+	SimpleDateFormat sdf;
+	String timeNow;
+	SMPanel panel;
 	
-	public DateAndClockWidget(int x, int y, int width, int hight) {
+	public DateAndClockWidget(int x, int y, int width, int hight, SMPanel panel) {
 		super(new Area(x, y, width, hight));
 		dh = new DateHandler();
+		this.panel = panel;
 	}
 	
 	public void render(Graphics g) {
 		super.render(g);
 		g.setColor(Color.WHITE);
+		dh = null;
+		dh = new DateHandler();
 		g.drawString(dh.getFullDate(), area.getxCoord()+10, area.getyCoord()+30);
-		g.drawString(LocalTime.now().toString(), area.getxCoord()+10, area.getyCoord()+10 );
+		g.drawString(getTime(), area.getxCoord()+10, area.getyCoord()+10 );
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		long lastUpdate = System.currentTimeMillis();
+		while(true) {
+			if(System.currentTimeMillis()-lastUpdate >= 1000) {
+				update();
+				lastUpdate = System.currentTimeMillis();
+			}
+		}
+	}
+	
+	public String getTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		String timeNow = sdf.format(new Date());
+		return timeNow;
+	}
+	
+	private void update() {
+		panel.repaint();
 	}
 }
