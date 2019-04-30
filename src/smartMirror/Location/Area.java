6274cfdb20ddb9a7c2ecd72623b5073fe13cfef3
Marvi <Marvi@192.168.0.11>
2019-04-30 21:29:19 +0200

    location update

---
+++ src/smartMirror/Location/Area.java
@@ -0,0 +1,9 @@
+package smartMirror.Location;
+
+public class Area extends Position{
+
+	public Area() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/smartMirror/Location/Position.java
@@ -0,0 +1,9 @@
+package smartMirror.Location;
+
+public class Position {
+
+	public Position() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
