a65e5a94a4a52dede6d308dc74a964a09613b2a5
linde9821 <lindner197@gmail.com>
2019-04-30 20:19:05 +0200

    test commit

---
+++ src/smartMIrror/SMManager/SMManager.java
@@ -0,0 +1,10 @@
+package smartMIrror.SMManager;
+
+public class SMManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+	}
+
+}
